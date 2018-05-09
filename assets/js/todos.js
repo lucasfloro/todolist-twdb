434a2dec779b1716f1cc40736c4b52aa17f8d181
Lucas Flóro <lucas.floro@gmail.com>
2018-05-08 22:40:07 -0300

    Marking Completion

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,17 @@
-console.log("connect");
+// Check off Specific Todos By Clicking
+
+$("li").click(function () {
+    $(this).toggleClass("completed");
+    // Esta linha acima é o jeito simples e rápido de fazer todo código abaixo
+    // if ($(this).css("color") === "rgb(128, 128, 128)") {
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"            
+    //     });
+    // } else {
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });            
+    // }
+});
